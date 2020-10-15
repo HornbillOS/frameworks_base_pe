f7968ad7499168bb1bf0bfad944ebb6ddff47fe0
nahnah <nandanrmenon@gmail.com>
2020-10-15 19:10:02 +0530

    Revert "SystemUI: add caffeine qs tile"
    This reverts commit 60b6c6eda05f9c2f33eb6c5abb4424167df5889d.

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -31,7 +31,6 @@ import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
-import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
@@ -178,8 +177,6 @@ public class QSFactoryImpl implements QSFactory {
             // Custom tiles.
             case "powershare":
                 return mPowerShareTileProvider.get();
-            case "caffeine":
-                 return new CaffeineTile(mHost);
         }
 
         // Intent tiles.
